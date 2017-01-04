32eedfed431faf0d1fe81787f152c3368f02ec62
clarenceenrique <egnis-hoxha@hotmail.com>
2017-01-04 20:37:39 +0100

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,60 @@
+var app = angular.module('myapp', []);
+app.controller('ctrl_1', function ($scope, $http){
+	var stocks=[];
+	$http.get('http://localhost:3000/getList').
+    	then(function(d){
+        	console.log(d.data);
+        	stocks=d.data;
+  	});
+    //simulate stock change
+    function changeStock(){
+    	stocks.forEach(function(stock){
+	    	stock.last = stock.open;
+	    	stock.high = stock.open;
+	    	stock.low = stock.open;
+	    	stock.ch = "";
+	    	stock.cc="";
+	    	stock.iconClass="";
+		
+	    	var index = Math.floor(Math.random() * stocks.length);
+	        var stock = stocks[index];
+
+	        var maxChange = stock.open * 0.005;
+	        var change = maxChange - Math.random() * maxChange * 2;
+	        var last;
+
+		    var change = Math.round(change * 100) / 100;
+		    var change = change === 0 ? 0.01 : change;
+
+		    last = stock.last + change;
+
+		    if (last > stock.open * 1.15 || last < stock.open * 0.85){
+		        change = -change;
+		        last = stock.last + change;
+		    }
+
+		    stock.change = change;
+		    stock.last = Math.round(last * 100) / 100;
+		    if (stock.last > stock.high){
+		        stock.high = stock.last;
+		        stock.cc="change-positive";
+		        stock.iconClass="glyphicon glyphicon-triangle-top"
+		        stock.ch="last-positive";
+		    }
+		    if(stock.last < stock.low){
+		        stock.low = stock.last;
+		        stock.cc="change-negative";
+		        stock.iconClass="glyphicon glyphicon-triangle-bottom"
+		        stock.ch="last-negative";
+		    }
+	    });
+	}
+    $scope.changeStock=changeStock;
+    $scope.changeStock();
+
+    setInterval(function(){
+		$scope.changeStock();
+		$scope.stocks=stocks;		
+		$scope.$apply();
+	},2000);
+});
---
+++ server.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// Add headers
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
+/**************************** API functions *****************************************/
+    //GET ALL list
+    app.get('/getList', function(req, res){
+        result=dataList;
+        res.json(result);
+    });  
+/**************************** END API functions **************************************/
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+
+
+
+// DATA SAMPLE
+var dataList=[
+    {symbol: "GM",  open: 38.87},
+    {symbol: "GE",  open: 25.40},
+    {symbol: "MCD", open: 97.05},
+    {symbol: "UAL", open: 69.45},
+    {symbol: "WMT", open: 83.24},
+    {symbol: "AAL", open: 55.76},
+    {symbol: "LLY", open: 76.12},
+    {symbol: "JPM", open: 61.75},
+    {symbol: "BAC", open: 15.84},
+    {symbol: "BA", open: 154.50}
+];
+
+/*
+    , last:"", change:"",high:"",low:""
+, last:"", change:"",high:"",low:""
+, last:"", change:"",high:"",low:""
+, last:"", change:"",high:"",low:""
+, last:"", change:"",high:"",low:""
+, last:"", change:"",high:"",low:""
+, last:"", change:"",high:"",low:""
+, last:"", change:"",high:"",low:""
+, last:"", change:"",high:"",low:""
+, last:"", change:"",high:"",low:""
+*/
